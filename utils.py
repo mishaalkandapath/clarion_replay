c7f6351b5928f1454f05b1ae717b5827a653d30f
mishaalkandapath <mishaalhere@gmail.com>
2025-04-01 16:23:25 -0400

    participant top-level attributes + modified fixed_rules class

---
+++ utils.py
@@ -0,0 +1,26 @@
+from pyClarion import FixedRules
+from pyClarion import Site, RuleStore, Choice, KeyForm, Family, Sort, Atom
+
+from typing import *
+
+class RuleWBLA(FixedRules):
+    main: Site
+    rules: RuleStore
+    choice: Choice
+    by: KeyForm
+    updated_main: Site
+
+    def __init__(self, 
+        name: str, 
+        p: Family,
+        r: Family,
+        c: Family, 
+        d: Family | Sort | Atom, 
+        v: Family | Sort,
+        *,
+        sd: float = 1.0
+    ) -> None:
+        
+        super().__init__(name, p=p, r=r, c=c, d=d, v=v, sd=sd)
+        self.bla_main = Site(self.rules.main.index, {}, 0.0)
+        self.choice.input = self.bla_main.main
